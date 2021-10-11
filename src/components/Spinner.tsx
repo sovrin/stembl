a530ed76edf7a871284877fe18b87cff0e1e6a99
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2021-10-11 20:45:54 +0200

    refactor, spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,28 +1,38 @@
 import React, {useState, useEffect} from 'react';
 import {Text} from 'ink';
 
+const FRAMES = ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'];
+
 /**
  *
  * @constructor
  */
-const Spinner = () => {
+const Spinner = ({isLoading}) => {
     const [frame, setFrame] = useState(0);
-    const frames = ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'];
     const interval = 80;
 
-    useEffect(() => {
+    /**
+     *
+     */
+    const onMount = () => {
         const timer = setInterval(() => (
-            setFrame((frame) => (frame + 1) % frames.length)
+            setFrame((frame) => (frame + 1) % FRAMES.length)
         ), interval);
 
         return () => {
             clearInterval(timer);
         };
-    }, []);
+    };
+
+    useEffect(onMount, [isLoading]);
+
+    if (!isLoading) {
+        return null;
+    }
 
     return (
         <Text color="blue">
-            {frames[frame]}
+            {FRAMES[frame]}
         </Text>
     );
 };
