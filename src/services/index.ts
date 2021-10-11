44a33b420115f3eb074b1e13d1c4c9bae39d2b47
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2021-10-11 20:45:16 +0200

    fix, remove obsolete services

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,3 @@
-export {default as container} from './container';
 export {default as date} from './date';
-export {default as io} from './io';
 export {default as router} from './router';
 export {default as timer} from './timer';
